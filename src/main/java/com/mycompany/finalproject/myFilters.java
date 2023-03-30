0b9f70bd452cdbb1c729f99ddc4ff35b17e74083
Danial <dchangez@uoguelph.ca>
2023-03-30 12:34:47 -0400

    Added List Filter

--- src/main/java/com/mycompany/finalproject/myFilters.java
+++ src/main/java/com/mycompany/finalproject/myFilters.java
@@ -1,12 +1,15 @@
 package com.mycompany.finalproject;
 
 import java.util.ArrayList;
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  *
  * @author danial
  */
-public class NameLengthFilters
+public class myFilters
 {
     public static void main(String[] args)
     {
@@ -29,7 +32,8 @@ public class NameLengthFilters
         ArrayList<String> output = new ArrayList<String>();
         for (int i = 0; i < entry.size(); i++)
         {
-            if (entry.get(i).contains("."))
+            File file = new File(entry.get(i));
+            if (file.isFile())
             {
                 switch (Operator)
                 {
@@ -58,6 +62,22 @@ public class NameLengthFilters
                             output.add(entry.get(i));
                         break;
                 }
+            }
+        }
+        return output;
+    }
+
+    public ArrayList<File> List(ArrayList<String> entry, int Max)
+    {
+        ArrayList<File> output = new ArrayList<File>();
+        for (int i = 0; i < entry.size(); i++)
+        {
+            File directory = new File(entry.get(i));
+            if (directory.isDirectory())
+            {
+                List<File> filesList = Arrays.asList(directory.listFiles());
+                for (int j = 0; j < Max; j++)
+                    output.add(filesList.get(j));
             }
         }
         return output;
